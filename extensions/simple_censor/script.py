e19ab08d3e9b8c330858f9a7c3a6053b709e8d35
Richard Lawrence <rarensu@tamu.edu>
2024-05-23 11:43:34 -0500

    Update simple censor, increased scope
    Update script.py

--- extensions/simple_censor/script.py
+++ extensions/simple_censor/script.py
@@ -2,7 +2,7 @@ from profanity_check import predict, predict_prob
 import re
 import numpy
 
-def output_modifier(string, state):
+def censor(string):
     sentences=re.split("(\.|\?|!|;|\n)",string)
     ratings=predict_prob(sentences)
     if (ratings>=0.1).any():
@@ -11,10 +11,19 @@ def output_modifier(string, state):
             if ratings[i]<0.1:
                 out+=sentences[i]
             else:
-                out+=" [redacted "+str(len(sentences[i]))+" chars]"
+                out+=" ["+str(len(sentences[i]))+" unsafe characters, not shown, please disregard]"
     else:
         out=string
     return out
+
+def output_modifier(string, state):
+    return censor(string)
+    
+def input_modifier(string, state):
+    return censor(string)
+    
+def chat_input_modifier(text, visible_text, state):
+    return censor(text), censor(visible_text)
     
 def state_modifier(state):
     state['stream']=False
